47cb9728707437bc5f69b409cdc7352cb8da117e
Taketomo Ohishi <takeweb@mac.com>
2021-04-16 09:36:08 +0900

    20210416学習分まで

--- gui_test/main.go
+++ gui_test/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
 func main() {
+	c := 0
 	a := app.New()
-
 	w := a.NewWindow("Hello")
+	l := widget.NewLabel("Hello Fyne!")
 	w.SetContent(
-		widget.NewLabel("Hello Fyne!"),
+		widget.NewVBox(
+			l,
+			widget.NewButton("Click me!", func() {
+				c++
+				l.SetText("count:" + strconv.Itoa(c))
+			}),
+		),
 	)
 	w.ShowAndRun()
 }
--- ho_func/go.mod
+++ ho_func/go.mod
@@ -1,5 +1,3 @@
 module ho_func
 
 go 1.16
-
-require fyne.io/fyne v1.4.3 // indirect
